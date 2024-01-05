6e43e49464662327db7bf39f9b509ae326130309
Cris Sousa <cristina.wep2013@gmail.com>
2024-01-05 20:21:14 -0300

    Desafio Concluídogit add Program.csgit add Program.csgit add Program.csgit add Program.csgit add Program.csgit add Program.csgit add Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+    
+    Console.WriteLine("Testando o Nokia:");
+    Nokia meuNokia = new Nokia("123456", "Nokia", "Modelo 1", "111111111", 64);
+    meuNokia.Ligar();
+    meuNokia.InstalarAplicativo("Whatsapp", "2.22.13.75", "WhatsApp, Inc.");
+    meuNokia.ReceberLigacao();
+    meuNokia.Desligar();
+
+    Console.WriteLine("\n");
+
+
+    Console.WriteLine("Testando o Iphone:");
+    Iphone meuIphone = new Iphone("4987", "Apple", "Modelo 2", "222222222", 126);
+    meuIphone.Ligar();
+    meuIphone.InstalarAplicativo("Telegram", "1.0.0", "Telegram, Inc.");
+    meuIphone.ReceberLigacao();
+    meuIphone.Desligar();
+  
+
